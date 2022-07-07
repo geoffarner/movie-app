6f572ab15d1e36266fbe9b2a77c10c2ccdaaf480
Geoffrey Arner <geoffarner5@gmail.com>
2022-07-06 21:56:09 -0500

    Added actor requests.http

--- movies_frontend.rb
+++ movies_frontend.rb
@@ -2,3 +2,6 @@ require "http"
 
 response = HTTP.get("http://localhost:3000/movies.json").parse(:json)
 pp response
+
+response = HTTP.get("http://localhost:3000/actors.json").parse(:json)
+pp response
