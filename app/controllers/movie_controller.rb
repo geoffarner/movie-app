99794d6e87f61224d2c9586e0f054e5067895ecc
Geoffrey Arner <geoffarner5@gmail.com>
2022-07-06 21:22:31 -0500

    Put in gem, movie controller, and movie route. Started request.http

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,11 +1,30 @@
 class MovieController < ApplicationController
   def index
-    movies = Movie.all
-    render json: movies.as_json
+    @movies = Movie.all
+    render template: "movies/index"
   end
 
   def show
-    movies = Movie.find_by(id: params[:id])
-    render json: movies.as_json
+    movies_id = params["id"]
+    @movies = Movie.find_by(id: movies_id)
+    render template: "movies/show"
+  end
+
+  def update
+    movie_id = params["id"]
+    @movies = Movie.find_by(id: movie_id)
+
+    @movies.title = params["title"] || @movies.title
+    @movies.year = params["year"] || @movies.year
+    @movies.plot = params["plot"] || @movies.plot
+    @movies.save
+    render template: "movies/show"
+  end
+
+  def destroy
+    movie_id = params["id"]
+    movie = Movie.find_by(id: movie_id)
+    movie.destroy
+    render json: { message: "Movie deleted" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get "/movies" => "movie#index"
+
   get "/movies/:id" => "movie#show"
+
+  post "/movies/" => "movie#create"
+
+  patch "/movies" => "movie#update"
+
+  delete "/movie/:id" => "movie#destroy"
 end
