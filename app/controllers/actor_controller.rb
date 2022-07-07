a5a19f2be75672520f8950cf871810e785b31028
Geoffrey Arner <geoffarner5@gmail.com>
2022-07-06 21:43:47 -0500

    Added actor controller

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -1,2 +1,41 @@
 class ActorController < ActionController::API
+  def index
+    @actors = Actor.all
+    render template: "actors/index"
+  end
+
+  def show
+    actor_id = params["id"]
+    @actors = Actor.find_by(id: actor_id)
+    render template: "actors/show"
+  end
+
+  def create
+    Actor.create(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"],
+    )
+    render template: "actors/show"
+  end
+
+  def update
+    actor_id = params["id"]
+    @actors = Actor.find_by(id: actor_id)
+
+    @actors.first_name = params["first_name"] || @actors.first_name
+    @actors.last_name = params["last_name"] || @actors.last_name
+    @actors.known_for = params["known_for"] || @actors.known_for
+    @actors.save
+    render template: "actors/show"
+  end
+
+  def destroy
+    actor_id = params["id"]
+    actor = Actor.find_by(id: actor_id)
+    actor.destroy
+    render json: { message: "Actor deleted" }
+  end
 end
+
+#first_name last_name known_for
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   get "/movies" => "movie#index"
-
+  get "/actors" => "actor#index"
+  ##
   get "/movies/:id" => "movie#show"
-
+  get "/actors" => "actor#show"
+  ##
   post "/movies/" => "movie#create"
-
+  post "/actors" => "actor#create"
+  ##
   patch "/movies" => "movie#update"
-
+  patch "/actors" => "actor#update"
+  ##
   delete "/movie/:id" => "movie#destroy"
+  delete "/actors" => "actor#destroy"
 end
